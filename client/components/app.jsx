3ca55671632086c888dfc56fcf9dbb7b2a4f45b6
Kevin <nhimkap4@gmail.com>
2020-07-16 16:25:46 -0700

    gave the app component an initial state for grades and defined a header component

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { grades: [] };
+  }
+
+  componentDidMount() {
+    const fetchRequest = fetch('/api/grades');
+    fetchRequest.then(response => {
+      response.json().then(data => {
+        this.setState({ grades: data });
+      });
+    });
+  }
+
   render() {
-    return null;
+    return (
+      <Header/>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <h1>Student Grade Table</h1>
+    );
   }
 }
 
