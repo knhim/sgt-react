476c1a8523848b7b26cc507189feee41fd0bc8f6
Kevin <nhimkap4@gmail.com>
2020-07-17 13:28:46 -0700

    added a form skeleton, and created a handleChange method

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './header';
 import GradeTable from './gradeTable';
+import GradeForm from './gradeForm';
 
 class App extends React.Component {
   constructor(props) {
@@ -55,6 +56,7 @@ class App extends React.Component {
       <div className="container">
         <Header averageGrade={averageGrade}/>
         <GradeTable grades={this.state.grades}/>
+        <GradeForm/>
       </div>
 
     );
--- client/components/gradeForm.jsx
+++ client/components/gradeForm.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
 
-function GradeForm(props) {
+export default class GradeForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { initialState: '' };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
+  handleChange(event) {
+    this.setState({ initialState: event.target.value });
+  }
+
+  render() {
+    return (
+      <>
+        <form>
+          <label>Name</label>
+          <input type="text" value={this.state.initialState} onChange={this.handleChange} />
+          <label>Course</label>
+          <input type="text" />
+          <label>Grade</label>
+          <input type="text" />
+        </form>
+        <button>Add</button>
+        <button>Cancel</button>
+      </>
+    );
+  }
 }
-
-export default GradeForm;
