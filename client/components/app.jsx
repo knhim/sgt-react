835a0efd6c707a0bd12eb50b2cbc442e881f0e41
Kevin <nhimkap4@gmail.com>
2020-07-19 18:03:42 -0700

    updated

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -24,7 +24,7 @@ class App extends React.Component {
     let total = 0;
     let avgGrade = 0;
     for (let i = 0; i < gradesArray.length; i++) {
-      total += gradesArray[i].grade;
+      total += parseInt(gradesArray[i].grade);
     }
     avgGrade = Math.ceil(total / gradesArray.length);
     return avgGrade.toString();
